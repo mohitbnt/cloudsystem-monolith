e61248d61729c986deb762144ee644c4774ca74a
Mohit Kumar <mohitbnt@gmail.com>
2026-08-23 21:55:44 +0530

    Update S3 backend key in Terraform configuration for consistency

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,7 +11,7 @@ terraform {
   }
   backend "s3" {
     bucket       = "repo-1338375073-tfstate"
-    key          = "medusa-monolith/global/terraform.tfstate"
+    key          = "cloudsystem-monolith/global/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
     encrypt      = true
